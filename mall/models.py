644a2b20bda544e5c1b67079f6684fe861a64fd1
luongtuandung <luongtuandung181209@gmail.com>
2023-07-16 19:36:20 +0700

    sdfgdfggd

--- mall/models.py
+++ mall/models.py
@@ -1,10 +1,18 @@
 from django.db import models
-
+type = (
+    ("technology", "Technology"),
+    ("fashion", "Fashion"),
+    ("healthbeauty", "Health and Beauty"),
+    ("toy", "Toy"),
+    ("fitness", "Fitness"),
+    ("food", "Food"),
+)
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=1000)
     description = models.TextField()
     image = models.ImageField(null=True)
     date = models.DateTimeField(auto_now_add=True)
+    kind = models.CharField(max_length = 100, choices = type, default="technology")
     cost = models.IntegerField()
 
